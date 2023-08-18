1f3fe22e3bf9acbc6dbfd0f76cb7efa1245baf44
Wheelyhog <wheelyhog@gmail.com>
2023-08-18 21:34:51 +0200

    update readme

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,9 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/vue_layout/',
   resolve: {
-    alias:{
+    alias: {
       '@': path.resolve(__dirname, 'src'),
     }
   }
